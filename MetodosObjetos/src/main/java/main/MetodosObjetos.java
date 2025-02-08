6a28b0cfc58d0f7f2426535802711ee66746b89e
Allan cardona <3004995740101@ingenieria.usac.edu.gt>
2025-02-07 20:21:37 -0600

    Creacion de Documentos

---
+++ MetodosObjetos/src/main/java/main/MetodosObjetos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+/**
+ *
+ * @author allan
+ */
+public class MetodosObjetos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
